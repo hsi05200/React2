b1c9b0abb53f657ab56cae0d5c0282f58572c5b7
stchwang <hsi05200@gmail.com>
2020-12-06 04:05:27 +0900

    주소록 기본세팅과 검색폼 기능구현을 위한 폼구성

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-//import logo from './logo.svg';
+//import logo from './logo.svg'; 이미지를 불러오는 방법
+
 import React from 'react';
 import "./App.css";
+import Contact from './components/Contact'
 
-class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      desc: ''
-    };
-  }
+class App extends React.Component {  
   render() {            
     return (                
       <div>
-        <button onClick={
-          ()=>{this.setState((state, props) => {
-              return {
-                name:'황성일',
-                desc:'React 학습에 오신걸 환영합니다.'
-              }
-            });
-          }}>클릭
-        </button>        
-        <h1>안녕하세요! {this.state.name}님!</h1>        
-        <div className="div">{this.state.desc}</div>
-      </div >
-   
+        <Contact/>
+      </div >   
     );
   }
 }
